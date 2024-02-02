a544d65545f80de331850017daa75ed0ea6aad76
matias astudillo <astudillomatias89@gmail.com>
2024-02-02 09:40:24 -0300

    Base

---
+++ src/main/java/com/ejemplo/act_practica/ActPracticaApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.act_practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActPracticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActPracticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemplo/act_practica/ActPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.act_practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActPracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
